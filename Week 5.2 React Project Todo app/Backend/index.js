dbcc3612feb5f07012328e951d1334d22bad9056
Anil Pal <anilcodes01@gmail.com>
2024-04-05 01:41:57 +0530

    Creating a todo app

--- Week 5.2 React Project Todo app/Backend/index.js
+++ Week 5.2 React Project Todo app/Backend/index.js
@@ -4,10 +4,11 @@
 const express = require("express");
 const app = express();
 import { createTodo, updateTodo } from "./types";
+const { todo } = require("./db");
 
 app.use(express.json());
 
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
   const createPayload = req.body;
   const parsePayload = createTodo.safeParse(createPayload);
   if (!parsePayload.success) {
@@ -17,16 +18,42 @@ app.post("/todo", (req, res) => {
     return;
   }
   // Put it in mongodb
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+  res.json({
+    msg: "Todo created!",
+  });
 });
 
-app.get("/todos", (req, res) => {});
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
 
-app.put("/completed", (req, res) => {
+  res.json({
+    todos,
+  });
+});
+
+app.put("/completed", async (req, res) => {
   const updatePayload = req.body;
   const parsePayload = updateTodo.safeParse(updatePayload);
   if (!parsePayload.success) {
     res.status(411).json({
       msg: "You sent the wrong inputs!",
     });
+    return;
   }
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+  res.json({
+    msg: "Todo marked as completed!",
+  });
 });
