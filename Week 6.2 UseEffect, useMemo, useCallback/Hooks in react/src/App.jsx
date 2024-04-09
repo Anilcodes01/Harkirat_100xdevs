d03b49bfb62afd6a8feb51994f49d6667c57a7fc
Anil Pal <anilcodes01@gmail.com>
2024-04-09 13:55:15 +0530

    react hooks

---
+++ Week 6.2 UseEffect, useMemo, useCallback/Hooks in react/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import axios from 'axios'
+
+
+function App() {
+const [todos, setTodos] = useState([])
+
+useEffect(() => {
+axios.get('https://sum-server.100xdevs.com/todos')
+.then(function(res) {
+  setTodos(res.data.todos)
+})
+}, [])
+
+  return (
+    <>
+      {todos.map(todo => <Todo title={todo.title} description={todo.description}/>)}
+    </>
+  )
+}
+
+
+function Todo({title, description}) {
+  return <div>
+    <h1>{title}</h1>
+    <h2>{description}</h2>
+  </div>
+}
+
+
+
+export default App
