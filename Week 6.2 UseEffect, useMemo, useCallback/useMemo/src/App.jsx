0d2fdf87e06b16df75856e36c80438400d5fa4aa
Anil Pal <anilcodes01@gmail.com>
2024-04-09 15:10:00 +0530

    useMemo

---
+++ Week 6.2 UseEffect, useMemo, useCallback/useMemo/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  const [inputValue, setInputValue] = useState(1)
+
+let count = 0;
+for(let i = 1; i<=inputValue; i++) {
+  count = count + i;
+}
+
+
+
+  return (
+   <div>
+
+<input onChange={function(e) {
+  setInputValue(e.target.value)
+
+}} placeholder={'Find sum from 1 to n'} ></input><br /><br />
+
+Sum from 1 to {inputValue} is {count}
+
+<br /><br />
+
+
+
+
+    <button onClick={function() {
+      setCounter(counter + 1)
+    }}>Counter {counter}</button>
+   </div>
+ 
+  )
+}
+
+export default App
