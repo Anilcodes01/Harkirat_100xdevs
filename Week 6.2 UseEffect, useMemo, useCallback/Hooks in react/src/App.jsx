79f703ca7a47036b74011fa58d229635a7851ab7
Anil Pal <anilcodes01@gmail.com>
2024-04-09 14:40:25 +0530

    Done with useEffect

--- Week 6.2 UseEffect, useMemo, useCallback/Hooks in react/src/App.jsx
+++ Week 6.2 UseEffect, useMemo, useCallback/Hooks in react/src/App.jsx
@@ -1,33 +1,115 @@
-import { useEffect } from "react"
-import { useState } from "react"
+// import { useEffect } from "react";
+// import { useState } from "react";
+// import axios from "axios";
+
+
+
+// function App() {
+//   const [selectedId, setSelectedId] = useState(1);
+
+//   return (
+//     <>
+//       <button onClick={function() {
+//         setSelectedId(1)
+//       }}>1</button>
+
+//       <button onClick={function() {
+//         setSelectedId(2)
+//       }}>2</button>
+//       <button onClick={function() {
+//         setSelectedId(3)
+//       }}>3</button>
+//       <button onClick={function() {
+//         setSelectedId(4)
+//       }}>4</button>
+//       <Todo id={selectedId} />
+//     </>
+//   );
+// }
+
+// function Todo({ id }) {
+//   const [todos, setTodos] = useState({});
+
+//   useEffect(() => {
+//     axios
+//       .get("https://sum-server.100xdevs.com/todo?id=" + id)
+//       .then(function (res) {
+//         setTodos(res.data.todo);
+//       });
+//   }, [id]);
+
+//   return (
+//     <div>
+//       id: {id}
+//       <h1>{todos.title}</h1>
+//       <h2>{todos.description}</h2>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import { useEffect, useState } from "react";
 import axios from 'axios'
 
-
 function App() {
-const [todos, setTodos] = useState([])
-
-useEffect(() => {
-axios.get('https://sum-server.100xdevs.com/todos')
-.then(function(res) {
-  setTodos(res.data.todos)
-})
-}, [])
+  const [selectedId, setSelectedId] = useState(1);
 
   return (
-    <>
-      {todos.map(todo => <Todo title={todo.title} description={todo.description}/>)}
-    </>
-  )
+    <div>
+      <button
+        onClick={function () {
+          setSelectedId(1);
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={function () {
+          setSelectedId(2);
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={function () {
+          setSelectedId(3);
+        }}
+      >
+        3
+      </button>
+      <button
+        onClick={function () {
+          setSelectedId(4);
+        }}
+      >
+        4
+      </button>
+      <Todo id={selectedId} />
+    </div>
+  );
 }
 
+function Todo({ id }) {
+  const [todos, setTodos] = useState({});
 
-function Todo({title, description}) {
-  return <div>
-    <h1>{title}</h1>
-    <h2>{description}</h2>
-  </div>
+  useEffect(() => {
+    axios
+      .get("https://sum-server.100xdevs.com/todo?id=" + id)
+      .then(function (res) {
+        setTodos(res.data.todo);
+      });
+  }, [id]);
+
+  return (
+    <div>
+      id: {id}
+      <h1>{todos.title}</h1>
+      <h2>{todos.description}</h2>
+    </div>
+  );
 }
 
-
-
-export default App
+export default App;
