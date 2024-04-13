59fb1f8e9d264638ae600a8103cdc5cd07829bfa
Anil Pal <anilcodes01@gmail.com>
2024-04-13 15:21:23 +0530

    Context api

--- Week 7.1 Context API, prop drilling/Prop Drilling/src/App.jsx
+++ Week 7.1 Context API, prop drilling/Prop Drilling/src/App.jsx
@@ -1,14 +1,47 @@
-
-import './App.css'
+import { useContext, useState } from "react"
+import { CountContext } from "./context";
 
 function App() {
-  
+  const [count, setCount] = useState(0);
 
+  // wrap anyone who wants to use the teleported value inside a provider
+  
   return (
-    <>
-      
-    </>
+    <div>
+      <CountContext.Provider value={count}>
+      <Count count={count} setCount={setCount} />
+
+      </CountContext.Provider>
+   
+    </div>
   )
+}
+
+function Count({ setCount}) {
+  return <div>
+    <CountRenderer />
+    <Buttons  setCount={setCount} />
+  </div>
+}
+
+function CountRenderer({}) {
+  const count = useContext(CountContext)
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({ setCount}) {
+  const count = useContext(CountContext)
+  return <div>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>Increase</button>
+
+    <button onClick={() => {
+      setCount(count - 1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
