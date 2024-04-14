804bb73ad51bb06e6b9d86520f8bc5f15520fe29
Anil Pal <anilcodes01@gmail.com>
2024-04-14 12:08:56 +0530

    Completed week 7

--- Week 7.2 Recoil, Context API/src/App.jsx
+++ Week 7.2 Recoil, Context API/src/App.jsx
@@ -1,11 +1,14 @@
 import { useContext } from "react";
 import { CountContext } from "./context";
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
-import { countAtom } from "./Store/atoms/Count";
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+import { countAtom, evenSelector } from "./Store/atoms/Count";
 
 function App() {
-  // wrap anyone who wants to use the teleported value inside a provider
-
   return (
     <div>
       <RecoilRoot>
@@ -16,26 +19,45 @@ function App() {
 }
 
 function Count() {
+  console.log("re-render");
   return (
     <div>
       <CountRenderer />
       <Buttons />
+     
     </div>
   );
 }
 
 function CountRenderer({}) {
   const count = useRecoilValue(countAtom);
-  return <div>{count}</div>;
+  return (
+    <div>
+      <b>{count}</b>
+      <EvenCountRenderer />
+    </div>
+  );
+}
+
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+  
+ 
+  return <div>
+    {isEven ? 'It is even' : 'It is odd'}
+  </div>
 }
 
 function Buttons() {
-  const [count, setCount] = useRecoilState(countAtom);
+  const setCount = useSetRecoilState(countAtom);
+  console.log("buttons re-renders");
   return (
     <div>
       <button
         onClick={() => {
-          setCount(count + 1);
+          setCount(function (count) {
+            return count + 1;
+          });
         }}
       >
         Increase
@@ -43,7 +65,9 @@ function Buttons() {
 
       <button
         onClick={() => {
-          setCount(count - 1);
+          setCount(function (count) {
+            return count - 1;
+          });
         }}
       >
         Decrease
