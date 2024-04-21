70fb54214cdd2c04e65f2a253b0669c81a85e979
Anil Pal <anilcodes01@gmail.com>
2024-04-21 23:10:49 +0530

    custom hooks

--- Week 8.3 Axios and Fetch/index.js
+++ Week 8.3 Axios and Fetch/index.js
@@ -1,6 +1,8 @@
-function main() {
-    fetch('https://sum-server.100xdevs.com/todos')
-    .then(async response => {
-        const json = await response.json();
-    })
+const axios = require('axios')
+
+async function main() {
+    const response = await axios.get('https://sum-server.100xdevs.com/todos');
+    console.log(response.data)
 }
+
+main();
