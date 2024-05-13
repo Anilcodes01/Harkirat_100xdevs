e93bcb5e2189d8b74e8e87782b6ec8840b3c1051
Anil Pal <anilcodes01@gmail.com>
2024-05-13 10:29:58 +0530

    functions in ts

---
+++ Week 9.2 Typescript Intro/node-app/a.js
@@ -0,0 +1,11 @@
+"use strict";
+let x = 1012;
+console.log(x);
+function Greet(name, lastname) {
+    console.log(`Hello ${name} ${lastname}`);
+}
+Greet('Anil', 'pal');
+function Sum(a, b) {
+    return a + b;
+}
+console.log(Sum(12, 43));
---
+++ Week 9.2 Typescript Intro/node-app/a.ts
@@ -0,0 +1,14 @@
+let x: number = 1012;
+console.log(x)
+
+function Greet(name: string, lastname: string) {
+    console.log(`Hello ${name} ${lastname}`)
+}
+
+Greet('Anil', 'pal')
+
+function Sum(a:number, b:number): number {
+    return a + b;
+}
+
+console.log(Sum(12,43))
